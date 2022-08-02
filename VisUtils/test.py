cb5ca1a0f9fadc6eceb4df34f6fb74d51fe5cd14
Kaikai Liu <kaikai.liu@sjsu.edu>
2022-08-02 01:02:48 -0700

    test X11 forwarding

---
+++ VisUtils/test.py
@@ -0,0 +1,11 @@
+
+#from mayavi import mlab
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('outputs/imageoutputs000000.png',cv2.IMREAD_GRAYSCALE)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
