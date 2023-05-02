5a5ece963cd17814ea282119d17ecc0d1344613d
Kaikai Liu <kaikai.liu@sjsu.edu>
2023-05-01 23:51:57 -0700

    add visualization with 3dbox

--- mydetector3d/tools/visonebatch.py
+++ mydetector3d/tools/visonebatch.py
@@ -10,13 +10,127 @@ import os
 import pickle 
 import torch
 
-from mydetector3d.tools.visual_utils.mayavivisualize_utils import visualize_pts, draw_lidar, draw_gt_boxes3d, draw_scenes #, pltlidar_with3dbox
+from mydetector3d.tools.visual_utils.mayavivisualize_utils import boxes_to_corners_3d, visualize_pts, draw_lidar, draw_gt_boxes3d, mydraw_scenes, draw_scenes #, pltlidar_with3dbox
+
+def roty(t):
+    """ Rotation about the y-axis. """
+    c = np.cos(t)
+    s = np.sin(t)
+    return np.array([[c, 0, s], [0, 1, 0], [-s, 0, c]])
+
+def compute_box_3d(obj, dataset='kitti'):
+    """ Takes an object3D
+        Returns:
+            corners_3d: (8,3) array in in rect camera coord.
+    """
+    #x-y-z: front-left-up (waymo) -> x_right-y_down-z_front(kitti)
+    # compute rotational matrix around yaw axis (camera coord y pointing to the bottom, thus Yaw axis is rotate y-axis)
+    R = roty(obj.ry)
+
+    # 3d bounding box dimensions: x, y, z correspond to l, w, h (waymo) -> l, h, w (kitti)
+    l = obj.l #x
+    w = obj.w #z
+    h = obj.h #y
+
+    # 3d bounding box corners
+    x_corners = [l / 2, l / 2, -l / 2, -l / 2, l / 2, l / 2, -l / 2, -l / 2]
+    y_corners = [0, 0, 0, 0, -h, -h, -h, -h]
+    z_corners = [w / 2, -w / 2, -w / 2, w / 2, w / 2, -w / 2, -w / 2, w / 2]
+
+    # rotate and translate 3d bounding box
+    corners_3d = np.dot(R, np.vstack([x_corners, y_corners, z_corners]))
+    # print corners_3d.shape
+    corners_3d[0, :] = corners_3d[0, :] + obj.t[0]
+    corners_3d[1, :] = corners_3d[1, :] + obj.t[1]
+    corners_3d[2, :] = corners_3d[2, :] + obj.t[2]
+    #print(corners_3d)
+    # print 'cornsers_3d: ', corners_3d
+    # only draw 3d bounding box for objs in front of the camera
+    # if np.any(corners_3d[2, :] < 0.1): #in Kitti, z axis is to the front, if z<0.1 means objs in back of camera
+    #     return np.transpose(corners_3d)
+    
+    return np.transpose(corners_3d)
+
+def draw_gt_boxes3d(
+    gt_boxes3d,
+    fig,
+    color=(1, 1, 1),
+    line_width=1,
+    draw_text=True,
+    text_scale=(1, 1, 1),
+    color_list=None,
+    label=""
+):
+    """ Draw 3D bounding boxes
+    Args:
+        gt_boxes3d: numpy array (n,8,3) for XYZs of the box corners
+        fig: mayavi figure handler
+        color: RGB value tuple in range (0,1), box line color
+        line_width: box line width
+        draw_text: boolean, if true, write box indices beside boxes
+        text_scale: three number tuple
+        color_list: a list of RGB tuple, if not None, overwrite color.
+    Returns:
+        fig: updated fig
+    """
+    num = len(gt_boxes3d)
+    for n in range(num):
+        b = gt_boxes3d[n]
+        if color_list is not None:
+            color = color_list[n]
+        if draw_text:
+            mlab.text3d(
+                b[4, 0],
+                b[4, 1],
+                b[4, 2],
+                label,
+                scale=text_scale,
+                color=color,
+                figure=fig,
+            )
+        for k in range(0, 4):
+            # http://docs.enthought.com/mayavi/mayavi/auto/mlab_helper_functions.html
+            i, j = k, (k + 1) % 4
+            mlab.plot3d(
+                [b[i, 0], b[j, 0]],
+                [b[i, 1], b[j, 1]],
+                [b[i, 2], b[j, 2]],
+                color=color,
+                tube_radius=None,
+                line_width=line_width,
+                figure=fig,
+            )
+
+            i, j = k + 4, (k + 1) % 4 + 4
+            mlab.plot3d(
+                [b[i, 0], b[j, 0]],
+                [b[i, 1], b[j, 1]],
+                [b[i, 2], b[j, 2]],
+                color=color,
+                tube_radius=None,
+                line_width=line_width,
+                figure=fig,
+            )
+
+            i, j = k, k + 4
+            mlab.plot3d(
+                [b[i, 0], b[j, 0]],
+                [b[i, 1], b[j, 1]],
+                [b[i, 2], b[j, 2]],
+                color=color,
+                tube_radius=None,
+                line_width=line_width,
+                figure=fig,
+            )
+    # mlab.show(1)
+    # mlab.view(azimuth=180, elevation=70, focalpoint=[ 12.0909996 , -1.04700089, -2.03249991], distance=62.0, figure=fig)
+    return fig
 
 if __name__ == "__main__":
     # Parser
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--batchpklfile_path", default='/home/lkk/Developer/data/waymokitti_myvoxelnext_onebatch_1.pkl', help="pkl file path"
+        "--batchpklfile_path", default='/home/lkk/Developer/data/mysecond_onebatch_1.pkl', help="pkl file path"
     )#'./data/waymokittisample'
     parser.add_argument(
         "--index", default="10", help="file index"
@@ -68,7 +182,23 @@ if __name__ == "__main__":
     print(pred_boxes.shape)
     print(pred_scores)
 
-    draw_scenes(idx_points, idx_gtboxes, pred_boxes)
+    import mayavi.mlab as mlab
+    fig = mlab.figure(
+        figure=None, bgcolor=(0, 0, 0), fgcolor=None, engine=None, size=(1000, 500)
+    )
+    point_cloud_range = [-75.2, -75.2, -2, 75.2, 75.2, 4] #[0, -40, -3, 70.4, 40, 1]
+    if not isinstance(idx_points, np.ndarray):
+        idx_points = idx_points.cpu().numpy()
+    draw_lidar(idx_points, fig=fig, pts_scale=5, pc_label=False, color_by_intensity=False, drawregion=True, point_cloud_range=point_cloud_range)
+    if idx_gtboxes is not None and not isinstance(idx_gtboxes, np.ndarray):
+        idx_gtboxes = idx_gtboxes.cpu().numpy()
+    #box3d_pts_3d = compute_box_3d(idx_gtboxes) #3d box coordinate=>get 8 points in camera rect, need 3DObject 
+    box3d_pts_3d = boxes_to_corners_3d(idx_gtboxes) #[42,8]->(42, 8, 3)
+    #colorlabel=INSTANCE3D_Color[obj.type]
+    draw_gt_boxes3d(box3d_pts_3d, fig=fig, color=(1, 1, 1), line_width=1, draw_text=False, label=None) #(n,8,3)
+    mlab.show()
+
+    mydraw_scenes(idx_points, idx_gtboxes, pred_boxes)
 
     print('done')
 
